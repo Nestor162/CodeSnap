8ed1261451390f91d20b2e9a2c390598023b45ea
Nestor162 <nestorcicardini@gmail.com>
2024-01-09 16:56:12 +0100

    Install missing playwright dependencies

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#Install playwright dependencies
+sudo apt-get update
+sudo apt-get install -y libicu66 libevent-2.1-7 libjpeg8 enchant libsecret-1-0 libffi7 libgles2
+
 #Before starting the server, make sure you have the playwright browser installed
 playwright install
 
